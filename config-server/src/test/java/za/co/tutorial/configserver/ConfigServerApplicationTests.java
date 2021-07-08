944d79b3af1668e520f7591a1d03dbac81d24629
Bright Mabuza <mabuza6@gmail.com>
2021-07-08 23:10:57 +0200

    "initial commit config server"

---
+++ config-server/src/test/java/za/co/tutorial/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.tutorial.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
